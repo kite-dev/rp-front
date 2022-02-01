0117663d2d13b0711d624ae02be13aef72d584a6
Julio Videla <julio.videla@kastor.cl>
2022-02-01 19:30:26 -0300

    Fix Deploy

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/rp-front'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/rp-front/index.html'));
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(port, (err) => {
