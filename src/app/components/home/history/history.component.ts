3bc566eb1395d8dd5ddab43d5d9ce53f166bdbe9
Julio Videla <julio.videla@kastor.cl>
2022-02-04 19:42:23 -0300

    FIX-DEPLOY

--- src/app/components/home/history/history.component.ts
+++ src/app/components/home/history/history.component.ts
@@ -12,6 +12,7 @@ export class HistoryComponent implements OnInit {
     autoClose: false,
     keepAfterRouteChange: false
   };
+  
   public dtOptions: DataTables.Settings = {
     pagingType: 'full_numbers',
     pageLength: 10,
